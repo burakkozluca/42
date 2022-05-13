31c7605499f3148db5395b31fa0dd87466ebca45
Burak Kozluca <83135082+burakkozluca@users.noreply.github.com>
2022-05-13 15:32:03 +0300

    second commit

---
+++ ft_printf/include/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkozluca <bkozluca@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 15:13:00 by bkozluca          #+#    #+#             */
+/*   Updated: 2022/05/09 15:17:00 by bkozluca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *str, ...);
+
+#endif
