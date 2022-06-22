3e4e4a0b942c77bc5477177061413aee04610f97
Burak Kozluca <83135082+burakkozluca@users.noreply.github.com>
2022-06-22 15:56:27 +0300

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,36 @@
 /*   By: bkozluca <bkozluca@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 11:49:13 by bkozluca          #+#    #+#             */
-/*   Updated: 2022/06/22 09:50:59 by bkozluca         ###   ########.fr       */
+/*   Updated: 2022/06/22 14:30:19 by bkozluca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0' || c == '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return (0);
+}
+//Tüm dosyayı oku sonra her satırı diziye at
+char *ft_read(int fd)
+{
+	static char* dizi;
+	int i;
+	char *ret;
+
+	i = 1;
+	while (i && !ft_strchr(ret, '\n'))
+	{
+		i = read(fd, dizi, BUFFERSIZE);
+	}
+	return(dizi);
+}
 
 int	ft_strlen(const char *str)
 {
@@ -82,35 +103,27 @@ char	*ft_strjoin1(char const *s1, char const *s2)
 	return (ptr_str);
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s != '\0' || c == '\0')
-	{
-		if (*s == c)
-			return ((char *)s);
-		s++;
-	}
-	return (0);
-}
-
 char *get_next_line(int fd)
 {
 	int		i;
-	int 	j;
-	char	*dizi;
+	//static char ı ret sonrası \n den sonra tutacak şekilde güncelleyen fonksiyon. yeni değer \n den sonrası
+	static char	*dizi;// static her çalıştığında yeni linedan başlayarak tutan pointer,
+	char		*ret;// tüm bilgileri tutan dizi içerisinden line '\n' a kadar dönen arrayi eşitleyip kullanıcıya döndür
 	char	c;
-	int 	buffer_size = 3;
+	int 	buffer_size = 2;
 	char 	*s;
 
-	dizi = (char *)malloc(ft_strlen(dizi));
+	//dizi = (char *)malloc(ft_strlen(dizi));
+	s = (char *)malloc(buffer_size + 1);
 	i = 1;
 	while (i)
 	{
 		if (buffer_size != 1)
 		{
-			i += read(fd, s, buffer_size);
+			i = read(fd, s, buffer_size);
 			//printf("%s",s);
 			dizi = ft_strjoin1(dizi, s);
+			free(s);
 			i--;
 			if (s[i] == '\n' || s[i] == '\0')
 				break ;
@@ -134,10 +147,12 @@ int main()
 
 	fd = open("oku.txt", 0);
 	printf("%s", get_next_line(fd));
-	printf("\n");
+	printf("%s", get_next_line(fd));
 	// printf("%s", get_next_line(fd));
 	// printf("%s", get_next_line(fd));
-	// printf("%s", get_next_line(fd));
+
+	int	read;
+	char *ret;
 
 	//get_next_line(fd);
 }
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE 42
+
+#endif
