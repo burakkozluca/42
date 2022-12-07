085ca504ab4b2509d63d6836e485cf839bcc348b
Burak Kozluca <bkozluca@k1m13s07.42kocaeli.com.tr>
2022-12-07 09:41:09 +0300

    burak

--- exam/Level2/ft_atoi/ft_atoi.c
+++ exam/Level2/ft_atoi/ft_atoi.c
@@ -2,14 +2,14 @@
 
 int	ft_atoi(char *s)
 {
-	int sign;
-	int r;
+	int	sign;
+	int	r;
 
-	while (*s <= 32)
+	while (*s <= ' ' || (*s <= 13 && *s >= 9))
 		s++;
-	if(*s == '-' || *s == '+')
+	if (*s == '-' || *s == '+')
 	{
-		if(*s == '-')
+		if (*s == '-')
 			sign = -1;
 		s++;
 	}
@@ -18,5 +18,5 @@ int	ft_atoi(char *s)
 		r = r * 10 + *s - '0';
 		s++;
 	}
-	return(sign * (int)r);
+	return (sign * (int)r);
 }
